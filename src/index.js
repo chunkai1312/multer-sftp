8b95a769fbe2a56a6739c5fff548d581be3f5409
Chun-Kai Wang <chunkai1312@gmail.com>
2020-02-04 10:57:35 +0800

    refactor: remove babel

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import SFTPStorage from './SFTPStorage'
-
-export default function (opts) {
-  return new SFTPStorage(opts)
-}
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto'
-
-export function getFilename (req, file, cb) {
-  crypto.pseudoRandomBytes(16, function (err, raw) {
-    cb(err, err ? undefined : raw.toString('hex'))
-  })
-}
